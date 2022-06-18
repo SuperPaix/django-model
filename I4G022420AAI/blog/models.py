4127c1ca644a66558d769e3f7ce9a00d5bfd8384
SuperPaix <101859488+SuperPaix@users.noreply.github.com>
2022-06-18 21:48:50 +0100

    Update models.py

--- I4G022420AAI/blog/models.py
+++ I4G022420AAI/blog/models.py
@@ -5,8 +5,6 @@ from django.db import models
 class post (models.Model):
   title = models.CharField(max_length=200)
   text = models.TextField()
-  author = models.ForeignKey(User,
-                            on_delete=models.CASCADE,
-                            related_name='blog_posts')
+  author = models.ForeignKey()
   created_date = models.DateTimeField(auto_now_add=True)
-  publshed_date = models.DateTimeField(default=timezone.now)
+  published_date = models.DateTimeField(default=timezone.now)
