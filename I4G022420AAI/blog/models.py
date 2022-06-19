6ee53eb66f8e37a99c2e308c26ac658f0d434e9c
Super_Paix <yawebelle@gmail.com>
2022-06-19 07:49:45 +0100

    makemigration command added

--- I4G022420AAI/blog/models.py
+++ I4G022420AAI/blog/models.py
@@ -1,12 +1,9 @@
-from time import timezone
 from django.db import models
 
 # Create your models here.
 class post (models.Model):
   title = models.CharField(max_length=200)
   text = models.TextField()
-  author = models.ForeignKey(User,
-                            on_delete=models.CASCADE,
-                            related_name='blog_posts')
+  author = models.ForeignKey(to='self', on_delete=models.CASCADE)
   created_date = models.DateTimeField(auto_now_add=True)
-  publshed_date = models.DateTimeField(default=timezone.now)
+  publshed_date = models.DateTimeField()
